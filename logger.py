77a86be14de6f58251584b93e4e56199055c90bf
BrnCosta <brunooc.bc@gmail.com>
2024-10-14 00:11:50 -0300

    Changing logger file place

--- logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
